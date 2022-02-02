73745751ff31057837746c754d4f57efab064a98
James Watson <>
2022-02-02 13:06:39 -0500

    feat(commands): add ArcadeDrive command

---
+++ src/main/java/com/maxtech/maxx/commands/ArcadeDrive.java
@@ -0,0 +1,23 @@
+package com.maxtech.maxx.commands;
+
+import com.maxtech.maxx.subsystems.DriveSubsystem;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+
+public class ArcadeDrive extends CommandBase {
+    private final double speed;
+    private final double degrees;
+    private final DriveSubsystem drivetrain;
+
+    public ArcadeDrive(double speed, double degrees, DriveSubsystem drivetrain) {
+        this.speed = speed;
+        this.degrees = degrees;
+        this.drivetrain = drivetrain;
+
+        addRequirements(drivetrain);
+    }
+
+    @Override
+    public void execute() {
+        drivetrain.arcade(speed, degrees);
+    }
+}
