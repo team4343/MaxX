9990316a819fa8e3ad8315afbc1bbdec6c7b2f41
MrBocking <professionaltutoringmarkham@gmail.com>
2022-02-06 16:11:09 -0500

    feat: add Basic.java

--- src/main/java/com/maxtech/maxx/subsystems/IntakeSubsystem.java
+++ src/main/java/com/maxtech/maxx/subsystems/IntakeSubsystem.java
@@ -3,14 +3,16 @@ package com.maxtech.maxx.subsystems;
 import com.maxtech.maxx.Constants;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel;
+import edu.wpi.first.wpilibj.motorcontrol.MotorController;
 import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class IntakeSubsystem extends SubsystemBase {
 
-    CANSparkMax leftIntakeMotor = new CANSparkMax(Constants.leftIntakeMotorID, CANSparkMaxLowLevel.MotorType.kBrushless);
-    CANSparkMax rightIntakeMotor = new CANSparkMax(Constants.rightIntakeMotorID, CANSparkMaxLowLevel.MotorType.kBrushless);
-    MotorControllerGroup intake = new MotorControllerGroup(leftIntakeMotor, rightIntakeMotor);
+
+    MotorController left1 = new CANSparkMax(Constants.leftIntakeMotorID, CANSparkMaxLowLevel.MotorType.kBrushless);
+    MotorController right1 = new CANSparkMax(Constants.rightIntakeMotorID, CANSparkMaxLowLevel.MotorType.kBrushless);
+    MotorControllerGroup intake = new MotorControllerGroup(left1, right1);
 
     public IntakeSubsystem() {
 
