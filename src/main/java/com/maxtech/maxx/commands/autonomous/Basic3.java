d891d31b08863dd21d0e3fb83a3d4ad6034f4844
MrBocking <professionaltutoringmarkham@gmail.com>
2022-02-07 14:40:29 -0500

    feat: add Basic.java

--- src/main/java/com/maxtech/maxx/commands/autonomous/Basic3.java
+++ src/main/java/com/maxtech/maxx/commands/autonomous/Basic3.java
@@ -3,13 +3,13 @@ package com.maxtech.maxx.commands.autonomous;
 import com.maxtech.maxx.subsystems.DriveSubsystem;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 
-public class Basic extends CommandBase{
+public class Basic3 extends CommandBase{
 
     private final double speed;
     private final double degrees;
     private final DriveSubsystem drivetrain;
 
-    public Basic(double degrees, double speed, DriveSubsystem drivetrain) {
+    public Basic3(double degrees, double speed, DriveSubsystem drivetrain) {
         this.speed = speed;
         this.degrees = degrees;
         this.drivetrain = drivetrain;
