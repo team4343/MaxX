8236473519ae74224e14c0ed2df181f77575cf24
R11K <95931562+R11K@users.noreply.github.com>
2022-02-15 17:46:14 -0500

    setting up intake subsystem

--- src/main/java/com/maxtech/maxx/Constants.java
+++ src/main/java/com/maxtech/maxx/Constants.java
@@ -15,6 +15,8 @@ public final class Constants {
     public static final int left2ID = 3;
     public static final int right1ID = 4;
     public static final int right2ID = 5;
+    public static final int PivotID = 6;
+    public static final int intakeid = 7;
 
     public static final class Drive {
         /**
