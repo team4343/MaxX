ee772c357b625daa038648864317cb1fd44d5c23
Bobbbay <abatterysingle@gmail.com>
2022-02-20 13:10:27 -0500

    fix: modify intake statemachine

--- src/main/java/com/maxtech/maxx/subsystems/Intake.java
+++ src/main/java/com/maxtech/maxx/subsystems/Intake.java
@@ -3,6 +3,7 @@ package com.maxtech.maxx.subsystems;
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.VictorSPX;
 import com.maxtech.lib.command.Subsystem;
+import com.maxtech.lib.logging.RobotLogger;
 import com.maxtech.lib.statemachines.StateMachine;
 import com.maxtech.lib.statemachines.StateMachineMeta;
 import com.maxtech.maxx.Constants;
@@ -20,31 +21,39 @@ public class Intake extends Subsystem {
     }
 
     private enum IntakeState {
-        Raised, Lowered,
+        Raising, Raised, Lowering, Lowered,
     }
 
     StateMachine<IntakeState> statemachine = new StateMachine<>("Intake", IntakeState.Raised);
 
     private Intake() {
-        statemachine.associateState(IntakeState.Raised, this::handleRaised);
-        statemachine.associateState(IntakeState.Lowered, this::handleLowered);
+        statemachine.associateState(IntakeState.Raised, this::handleRaising);
+        statemachine.associateState(IntakeState.Lowering, this::handleLowering);
         statemachine.start();
     }
 
     private final VictorSPX intakeMotor = new VictorSPX(Constants.intakeID);
 
-    private void handleRaised(StateMachineMeta m) {
-
+    /** We want to raise the intake. */
+    private void handleRaising(StateMachineMeta m) {
     }
 
-    private void handleLowered(StateMachineMeta m) {
-
+    /** We want to lower the intake. */
+    private void handleLowering(StateMachineMeta m) {
     }
 
     @Override
     public void sendTelemetry(String prefix) {
         SmartDashboard.putNumber(prefix + "voltage", intakeMotor.getBusVoltage());
         SmartDashboard.putNumber(prefix + "temperature", intakeMotor.getTemperature());
+    }
+
+    public void raise() {
+        statemachine.toState(IntakeState.Raising);
+    }
+
+    public void lower() {
+        statemachine.toState(IntakeState.Lowering);
     }
 
     public void start() {
