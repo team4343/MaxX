bb00b7215be5d8b97d0a576c3d949554d6d8613b
Bobbbay <abatterysingle@gmail.com>
2022-04-08 12:36:14 -0400

    feat: simple PathPlanner 3-ball autonomous path

--- src/main/java/com/maxtech/maxx/commands/porcelain/autonomous/advanced/ThreeBallFromFender.java
+++ src/main/java/com/maxtech/maxx/commands/porcelain/autonomous/advanced/ThreeBallFromFender.java
@@ -4,14 +4,15 @@ import com.maxtech.lib.command.AutonomousSequentialCommandGroup;
 import com.maxtech.maxx.commands.plumbing.autonomous.RunTrajectory;
 import com.maxtech.maxx.commands.porcelain.intake.SetIntake;
 import com.maxtech.maxx.commands.porcelain.shooter.ShootHighFor;
+import com.pathplanner.lib.PathPlanner;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.trajectory.Trajectory;
 import edu.wpi.first.wpilibj2.command.ParallelDeadlineGroup;
 
 public class ThreeBallFromFender extends AutonomousSequentialCommandGroup {
-    Trajectory start = loadPathweaverTrajectory("paths/simple.fender2.start.wpilib.json");
-    Trajectory middle = loadPathweaverTrajectory("paths/simple.fender2.middle.wpilib.json");
-    Trajectory end = loadPathweaverTrajectory("paths/simple.fender2.end.wpilib.json");
+    Trajectory start = PathPlanner.loadPath("Right Fender.0", 5, 2);
+    Trajectory middle = PathPlanner.loadPath("Right Fender.1", 5, 2);
+    Trajectory end = PathPlanner.loadPath("Right Fender.2", 5, 2);
 
     public ThreeBallFromFender() {
         addCommands(
